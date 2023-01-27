b882026076a67c0a5534f05477eadd87d1d7d92f
Stanislav Zeman <zemanstanislav01@gmail.com>
2023-01-27 14:07:10 +0100

    Implementing searchFilters

--- packages/eshop/src/utils/filters/searchFIlters.ts
+++ packages/eshop/src/utils/filters/searchFIlters.ts
@@ -1,18 +1,43 @@
-import { GetBooksRequest } from "grpc-ts/eshop_pb";
+import { GetBooksRequest, BookFilters, Pagination } from "grpc-ts/eshop_pb";
 
-const input = new URLSearchParams();
-
-input.append("search", "test");
-input.append("kokot", "on");
-
-/// ->>>>>
+export const calculateOffset = (page_number: number, limit: number) => {
+  return page_number * limit;
+}
 
 export const searchFilters = (input: URLSearchParams): GetBooksRequest => {
-  const res = new GetBooksRequest();
+  const filters = new BookFilters();
+  const pagination = new Pagination();
+  const limit = 16;
 
-  [...input.values()]; //?
+  for (const [key, value] of input.entries()) {
+    switch (key) {
+      case "author_name":
+        filters.setAuthorName(value);
+        break;
+      case "book_name":
+        filters.setName(value);
+        break;
+      case "category":
+        filters.setTag(value);
+        break;
+      case "price_to":
+        filters.setPriceto(+value);
+        break;
+      case "price_from":
+        filters.setPricefrom(+value);
+        break;
+      case "page":
+        pagination.setLimit(limit);
+        pagination.setOffset(calculateOffset(+value, limit));
+        break;
+      default:
+        break;
+    }
+  }
 
-  return res;
+  const request = new GetBooksRequest();
+  request.setFilters(filters);
+  request.setPagination(pagination);
+
+  return request;
 };
-
-searchFilters(input);
