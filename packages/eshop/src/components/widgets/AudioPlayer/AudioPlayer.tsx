224b8b22fadfe776a44739c9a8d10bfc139dafe3
Stanislav Zeman <zemanstanislav01@gmail.com>
2023-01-30 15:52:52 +0100

    feat: making player wider

--- packages/eshop/src/components/widgets/AudioPlayer/AudioPlayer.tsx
+++ packages/eshop/src/components/widgets/AudioPlayer/AudioPlayer.tsx
@@ -36,7 +36,7 @@ export const AudioPlayer: AudioPlayerType = ({ book }) => {
 
   return (
   <div class="min-h-screen flex flex-col items-center justify-center p-8">
-    <div class="max-w-xl bg-white rounded-lg shadow-lg overflow-hidden w-96">
+    <div class="max-w-xl bg-white rounded-lg shadow-lg overflow-hidden w-full">
       <div class="relative">
         <img
           src={book.cover_url}
@@ -46,36 +46,12 @@ export const AudioPlayer: AudioPlayerType = ({ book }) => {
           <span class="opacity-70">{book.name}</span>
         </div>
       </div>
-      {/* <div>
-        <div class="relative h-1 bg-gray-200">
-          <div class="absolute h-full w-1/2 bg-green-500 flex items-center justify-end">
-            <div class="rounded-full w-3 h-3 bg-white shadow"></div>
-          </div>
-        </div>
-      </div> */}
       <div class="flex justify-between text-xs font-semibold text-gray-500 px-4 py-2">
         <video autoplay controls class="w-full h-10" src={currectSource()}>
         </video>
-        {/* <div>
-          1:50
-        </div>
-        <div class="flex space-x-3 p-2">
-          <button class="focus:outline-none">
-            <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="19 20 9 12 19 4 19 20"></polygon><line x1="5" y1="19" x2="5" y2="5"></line></svg>
-          </button>
-          <button class="rounded-full w-8 h-8 flex items-center justify-center pl-0.5 ring-2 ring-gray-100 focus:outline-none">
-            <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
-          </button>
-          <button class="focus:outline-none">
-            <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 4 15 12 5 20 5 4"></polygon><line x1="19" y1="5" x2="19" y2="19"></line></svg>
-          </button>
-        </div>
-        <div>
-          3:00
-        </div> */}
       </div>
       <ul class="text-xs sm:text-base divide-y border-t cursor-default text-gray-500">
-      <For each={chapters}>{(chapter, i) =>
+      <For each={chapters}>{chapter =>
         <li class="flex items-center space-x-3 hover:bg-gray-100">
         <button class="p-3 hover:bg-green-500 group focus:outline-none" onClick={() => changeSource(chapter.link)}>
           <svg class="w-4 h-4 group-hover:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -85,18 +61,8 @@ export const AudioPlayer: AudioPlayerType = ({ book }) => {
         <div class="flex-1">
           {chapter.chapter.chapter_name}
         </div>
-        {/* <video controls class="w-full h-6">
-          <source src="https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/286/make-me-move-feat-karra-1586948725-C9SPOA3UZn.mp3" type="audio/mpeg">
-          </source>
-        </video> */}
-        {/* <button class="focus:outline-none pr-4 group">
-          <svg class="w-4 h-4 group-hover:text-green-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/>
-          </svg>
-        </button> */}
       </li>
-      }
-      </For>
+      }</For>
       </ul>
     </div>
   </div>)
