3dd5c23643814bb8ebae28c244da590c83e6d68e
Jakub Suchanek <524721@mail.muni.cz>
2023-01-30 12:57:42 +0100

    Astro disable next page when its not supposed to be used

--- packages/eshop/src/utils/filters/searchFilters.ts
+++ packages/eshop/src/utils/filters/searchFilters.ts
@@ -4,17 +4,18 @@ export const calculateOffset = (page_number: number, limit: number) => {
   return (page_number - 1) * limit;
 };
 
+export const PAGE_LIMIT = 12;
+
 export const searchFilters = (input: URLSearchParams): grpc.GetBooksRequest => {
   const request = new grpc.GetBooksRequest();
   const filters = new grpc.BookFilters();
   request.setFilters(filters);
-  const limit = 16;
 
   const pagination = new grpc.Pagination();
   request.setPagination(pagination);
 
-  pagination.setLimit(limit);
-  pagination.setOffset(calculateOffset(1, limit));
+  pagination.setLimit(PAGE_LIMIT);
+  pagination.setOffset(calculateOffset(1, PAGE_LIMIT));
 
   for (const [key, value] of input.entries()) {
     switch (key) {
@@ -38,7 +39,7 @@ export const searchFilters = (input: URLSearchParams): grpc.GetBooksRequest => {
         filters.setPricefrom(+value * 100);
         break;
       case "page":
-        pagination.setOffset(calculateOffset(+value, limit));
+        pagination.setOffset(calculateOffset(+value, PAGE_LIMIT));
         break;
       default:
         break;
