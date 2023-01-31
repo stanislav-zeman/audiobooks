3ea27df2be2cf262809ea26fac5a7921d8a0a540
Filip Kaštovský <devfilipkastovsky@gmail.com>
2023-01-31 03:11:08 +0100

    fix: shitidick serverless cant resolve syntheticdefaultimports

--- packages/eshop/src/pages/api/purchase/[book_id].ts
+++ packages/eshop/src/pages/api/purchase/[book_id].ts
@@ -1,12 +1,15 @@
 import type { APIRoute } from "astro";
 import { grpcClient } from "@utils/grpc/client";
 import { promise } from "@utils/grpc/promise";
-import { BuyBookRequest } from "grpc-ts/eshop_pb";
+import grpc from "grpc-ts/eshop_pb";
 import { includeToken } from "@utils/grpc/token";
 
-export const get: APIRoute = async ({ params, redirect,cookies }) => {
-  const buyBook = promise(grpcClient.buyBook.bind(grpcClient), includeToken(cookies.get("token").value));
-  const request = new BuyBookRequest();
+export const get: APIRoute = async ({ params, redirect, cookies }) => {
+  const buyBook = promise(
+    grpcClient.buyBook.bind(grpcClient),
+    includeToken(cookies.get("token").value)
+  );
+  const request = new grpc.BuyBookRequest();
 
   request.setBookId(params.book_id as string);
   await buyBook(request);
